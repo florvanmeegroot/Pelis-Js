63a1e2cdad9f5d7d4a0d431c8deb3fef8d880e3c
florvanmeegroot <florvanmeegroot@gmail.com>
2025-08-15 11:46:09 -0300

    cambios js y css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 // Prompt para obtener los datos del usuario
 let nombre = prompt("Por favor, ingresa tu nombre:");
 
-let edad = prompt("Hola " + nombre + ", ¿cuántos años tenés?");
+let añoNacimiento = prompt("Hola " + nombre + ", ¿En qué año naciste?");
 
-console.log("El usuario se llama: " + nombre + " y tiene: " + edad + " años"); // Lo imprimo por consola
+console.log("El usuario se llama: " + nombre + " y nacio en " + añoNacimiento ); // Lo imprimo por consola
 
 let mensaje = "Hola " + nombre; // Defino el mensaje
-document.getElementById("saludo").textContent = mensaje; // Saludo en el h1 del header
+document.getElementById("saludo").textContent = mensaje; // Saludo en el header
 
-console.log(mensaje); // Lo imprimo por consola
+console.log(mensaje); // Imprimo tmb por consola el saludo que esta en el header
+
+// Calcular la edad de la persona 
+const añoActual = new Date().getFullYear();
+console.log("Estamos en el: " + añoActual); //obtengo año actual y lo imprimo por consola
+
+let edad = añoActual - añoNacimiento; //operacion para calcular edad
+
+console.log ("El usuario " + nombre + " tiene " + edad + " años"); //imprimo la edad por consola
